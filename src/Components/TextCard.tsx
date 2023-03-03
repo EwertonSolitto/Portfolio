4820a89a13f297e497e19014db5ee574b02c1fbe
Ewerton <ewerton2004@yahoo.com.br>
2023-03-03 01:11:12 -0300

    feat(textcard): made the textcard with the right design

--- src/Components/TextCard.tsx
+++ src/Components/TextCard.tsx
@@ -1,9 +1,12 @@
 export default function TextCard () {
   return (
     <div className="text-card">
-     
-      <div className="front-card"></div>
-       <div className="content-card">a</div>
+      <div className="front-card">
+        <p>Olá</p>
+        <p>Bem vindo ao meu portfólio!</p>
+        <p>Eu sou o Ewerton Solitto.</p>
+        <p>Programador Fullstack</p>
+      </div>
       <div className="back-card"></div>
     </div>
   )
