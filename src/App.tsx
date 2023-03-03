bfce914adf1e21e278295822b891d243b5cffa37
Ewerton <ewerton2004@yahoo.com.br>
2023-03-03 18:34:04 -0300

    feat(textcard): create a way to have several texts and buttons to the textcard

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import TextCard from "./Components/TextCard";
 import './styles/css/index.css'
 
 function App() {
+  const mainText = [
+    {name: 'Olá'}, 
+    {name: 'Bem vindo ao meu portfólio!'}, 
+    {name: 'Eu sou o Ewerton Solitto.'}, 
+    {name: 'Programador Fullstack'}
+  ]
+  
   return (
     <div className="app">
       < Header/>
       
       <main className="home-main">
-        < TextCard/>
+        < TextCard size="10" paragraphs={mainText}/>
+
       </main>
     </div>
   );
--- src/Components/TextCard.tsx
+++ src/Components/TextCard.tsx
@@ -1,13 +1,22 @@
-export default function TextCard () {
+import TextCardProps from "../Interfaces/TextCardProps";
+
+export default function TextCard (props: TextCardProps) {
+  const {size, paragraphs, a} = props
+
   return (
     <div className="text-card">
-      <div className="front-card">
-        <p>Olá</p>
-        <p>Bem vindo ao meu portfólio!</p>
-        <p>Eu sou o Ewerton Solitto.</p>
-        <p>Programador Fullstack</p>
+      <div className={`front-card ${size}`}>
+        <div className="text-card-content">
+          {paragraphs?.map(({name, style}) => {
+              return <p className={style}>{name}</p>
+            })}
+
+          {a?.map(({name, url, style}) => {
+              return <a href={url} className={style}>{name}</a>
+            })}
+        </div>
       </div>
-      <div className="back-card"></div>
+      <div className={`back-card ${size}`}></div>
     </div>
   )
 }
