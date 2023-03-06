c6d854d2e6f8a68c8df5a4d7cd71eaef68448745
Ewerton <ewerton2004@yahoo.com.br>
2023-03-06 17:00:04 -0300

    perf(textcard): add title and create consts for each html tag to make the code cleaner

--- src/Components/TextCard.tsx
+++ src/Components/TextCard.tsx
@@ -2,22 +2,40 @@ import TextCardProps from "../interfaces/TextCardProps";
 import LinkButton from "./Button";
 
 export default function TextCard (props: TextCardProps) {
-  const {side, paragraphs, a} = props
+  const {side, title, paragraphs, a} = props
+
+  const h2 = title ? 
+    <h2 className={title.style}>
+      {title.content}
+    </h2>
+  : ''
+  
+  const paragraphDiv = paragraphs ?
+  <div className="paragraph-div">
+    {paragraphs.map(({content, style = ''}, index) => {
+      return <p key={index} className={style}>{content}</p>
+    })} 
+  </div>
+  :  ''
+
+  const buttonsDiv = a ? 
+    <div className="buttons-div">
+      {a.map(({content, link, style = '', svg}, index) => {
+        return <LinkButton key={index} link={link} style={style} content={content} svg={svg}/>
+      })}
+    </div> 
+    : ''
 
   return (
     <div className={`text-card ${side}`}>
       <div className={`front-card ${side}`}>
         <div className="text-card-content">
-          
-          {paragraphs?.map(({name, style = ''}, index) => {
-              return <p key={index} className={style}>{name}</p>
-            })}
 
-          <div className="buttons-div">
-            {a?.map(({content, link, style = '', svg}, index) => {
-              return <LinkButton key={index} link={link} style={style} content={content} svg={svg}/>
-            })}
-          </div>
+          {h2 ? h2 : ''}
+          
+          {paragraphDiv ? paragraphDiv : ''}
+
+          {buttonsDiv ? buttonsDiv : ''}
 
         </div>
       </div>
