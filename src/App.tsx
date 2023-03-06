9c3d67cc6439502b6d2da718cae808683e71b63e
Ewerton <ewerton2004@yahoo.com.br>
2023-03-06 17:40:21 -0300

    feat(about): create styles for about

--- src/App.tsx
+++ src/App.tsx
@@ -23,19 +23,23 @@ function App() {
     title: {content: 'Sobre'},
     text: [
       {content: 
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus sapien quis metus cursus fermentum. Duis id sapien id magna cursus porttitor. Nulla viverra felis at posuere cursus. Maecenas condimentum volutpat hendrerit. Duis consequat felis quis volutpat aliquet. Nullam quis porttitor turpis. Aliquam erat volutpat.'
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus sapien quis metus cursus fermentum. Duis id sapien id magna cursus porttitor. Nulla viverra felis at posuere cursus. Maecenas condimentum volutpat hendrerit. Duis consequat felis quis volutpat aliquet. Nullam quis porttitor turpis. Aliquam erat volutpat.',
+        style: 'text'
       },
       {content: 
-        'Nullam venenatis augue in metus euismod tempus. Sed a varius eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean at magna pellentesque, hendrerit ultrices, in ultrices orci consequat. Maecenas eget congue nulla.'
+        'Nullam venenatis augue in metus euismod tempus. Sed a varius eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean at magna pellentesque, hendrerit ultrices, in ultrices orci consequat. Maecenas eget congue nulla.',
+        style: 'text'
       }]
   }
   
   return (
     <div className="app">
       < Header/>
+
       <main className="home-main">
         < TextCard side="left" paragraphs={mainProps.text} a={mainProps.buttons} />
       </main>
+      
       <section className="about">
         < TextCard side="right" title={aboutProps.title} paragraphs={aboutProps.text} />
       </section>
