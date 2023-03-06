95325f6fd91415d02dc70e0ccc2fb3369fd97bc8
Ewerton <ewerton2004@yahoo.com.br>
2023-03-05 22:20:39 -0300

    style(header): change the white burguer icon into the list from phosphor icons

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,11 @@
-import { WhiteBurger } from "../assets/WhiteBurger";
+import { List } from "@phosphor-icons/react";
 
 export default function Header () {
  
   return (
     <header>
       <a href="#main"><h1><span className="font-blue">{'<'}</span>Portfólio<span className="font-blue">{'/>'}</span></h1></a>
-      <button>< WhiteBurger width={48} height={48}/></button>
+      <button><List size={48} color="#e8e8e8" weight="regular" /></button>
     </header>
   )
 }
