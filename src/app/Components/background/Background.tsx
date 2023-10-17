e7963dcd5bbf7ad1068c62e55146de7b174dc146
Ewerton <ewerton2004@yahoo.com.br>
2023-10-17 12:44:26 -0300

    feat(background): change some p tags to phosphor icons

--- src/app/Components/background/Background.tsx
+++ src/app/Components/background/Background.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import Image from "next/image";
 import Circle from "./background-components/Circle";
 import Points from "./background-components/Points";
 import Wave1 from "./background-components/Wave1";
 import Wave2 from "./background-components/Wave2";
+import { Code, BracketsSquare, BracketsCurly } from "@phosphor-icons/react";
 
 export default function Background() {
   return (
@@ -13,9 +16,9 @@ export default function Background() {
       <Points styles="third-points" quantity={3}/>
       <Wave1 />
       <p className="html">&lt;html&gt;</p>
-      <p className="empty-tag">&lt;/&gt;</p>
-      <p className="empty-array space">[]</p>
-      <p className="empty-object space">{'{}'}</p>
+      <Code color="#272D37" weight="regular" size={64} className="code"/>
+      <BracketsSquare color="#272D37" weight="regular" size={64} className="array"/>
+      <BracketsCurly color="#272D37" weight="regular" size={64} className="object"/>
       <Circle styles="second-circle reverse"/> 
       <Points styles="fourth-points" quantity={2}/>
       <p className="hello-world">console.log(&quot;Hello World!&quot;)</p>
