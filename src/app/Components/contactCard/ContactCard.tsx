4c500b1ad769f9621886866ca89c290c09de3494
Ewerton <ewerton2004@yahoo.com.br>
2023-10-17 13:17:36 -0300

    fix(autocomplete missing): put autocomplete in input tags

--- src/app/Components/contactCard/ContactCard.tsx
+++ src/app/Components/contactCard/ContactCard.tsx
@@ -51,6 +51,7 @@ export default function ContactCard(props: ContactCardProps) {
             value={name} 
             onChange={e => setName(e.target.value)}
             disabled={form.close && true}
+            autoComplete="on"
           />
         </div>
         
@@ -65,6 +66,7 @@ export default function ContactCard(props: ContactCardProps) {
             value={email}
             onChange={e => setEmail(e.target.value)}
             disabled={form.close && true}
+            autoComplete="on"
           />  
         </div>
 
@@ -82,6 +84,7 @@ export default function ContactCard(props: ContactCardProps) {
             value={phone}
             onChange={e => setPhone(e.target.value)}
             disabled={form.close && true}
+            autoComplete="on"
           />
         </div>
         
