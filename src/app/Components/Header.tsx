d304783384da5c6c6570c9174ba5138b561fc6ab
Ewerton <ewerton2004@yahoo.com.br>
2023-10-18 17:13:01 -0300

    refactor(header): remove header duplicated

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -6,48 +6,37 @@ import Logo from "./Logo";
 export default function Header () {
   const [showMenu, setShowMenu] = useState(false)
 
-  const logo = <a href="#home" className="logo" onClick={() => {setShowMenu(false)}}><h1><Logo /></h1></a>
-
   const burgerButton = <button onClick={() => setShowMenu(true)} className="burger"><List size={48} color="#e8e8e8" weight="regular" /></button>
 
   const xButton = <button onClick={() => setShowMenu(false)} className="x"><X size={48} color="#e8e8e8" weight="regular" /></button>
 
-  const navigation = 
-  (
-    <div className="opened-nav">
-      <nav>
-        <ul>
-          <li><a href="#home" onClick={() => setShowMenu(false)}>Home</a></li>
-          <li><a href="#sobre" onClick={() => setShowMenu(false)}>Sobre</a></li>
-          <li><a href="#habilidades" onClick={() => setShowMenu(false)}>Habilidades</a></li>
-          <li><a href="#projetos" onClick={() => setShowMenu(false)}>Projetos</a></li>
-          <li><a href="#contato" onClick={() => setShowMenu(false)}>Contato</a></li>
-        </ul>
-      </nav>
-    </div>
-  )
-  const select = 
-  (  
-    <div className="select-div">
-      <select name="language" id="language">
-        <option className="option" value="portuguese">Português</option>
-        <option className="option" value="english">Inglês</option>
-      </select>
-    </div>
-  )
-
   return (
     <header className={showMenu ? 'opened-menu' : ''}>
-        <div className='header mobile-header'>
-          {logo}
+        <div className='header'>
+          <a href="#home" className="logo" onClick={() => {setShowMenu(false)}}>
+            <h1><Logo /></h1>
+          </a>
+          
           {showMenu ? xButton : burgerButton}
-          {navigation}
-          {select}
-        </div>
-        <div className='header desktop-header'>
-          {logo}
-          {navigation}
-          {select}
+          
+          <div className="opened-nav">
+            <nav>
+              <ul>
+                <li><a href="#home" onClick={() => setShowMenu(false)}>Home</a></li>
+                <li><a href="#sobre" onClick={() => setShowMenu(false)}>Sobre</a></li>
+                <li><a href="#habilidades" onClick={() => setShowMenu(false)}>Habilidades</a></li>
+                <li><a href="#projetos" onClick={() => setShowMenu(false)}>Projetos</a></li>
+                <li><a href="#contato" onClick={() => setShowMenu(false)}>Contato</a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="select-div">
+            <select name="language" id="language">
+              <option className="option" value="portuguese">Português</option>
+              <option className="option" value="english">Inglês</option>
+            </select>
+          </div>
         </div>
     </header>
   )
