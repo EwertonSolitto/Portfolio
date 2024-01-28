a92c6e58392e656f0cf9465c9ed45cfbe0f125b9
Ewerton <ewerton2004@yahoo.com.br>
2024-01-28 11:46:00 -0300

    feat(background): add some circles on the background

--- src/app/Components/background/Background.tsx
+++ src/app/Components/background/Background.tsx
@@ -12,23 +12,36 @@ export default function Background() {
   return (
     <div className="background-elements" aria-hidden>
       <Circle styles="first-circle"/> 
+      <Circle styles="second-circle reverse"/> 
+      <Circle styles="third-circle no-background"/>
+      <Circle styles="fourth-circle" />
+      <Circle styles="fifth-circle gray-background gray-reverse" />
+      <Circle styles="sixth-circle gray-no-background" />
+      <Circle styles="seventh-circle gray-background" />
+      <Circle styles="eighth-circle white-background" />
+      <Circle styles="ninth-circle white-background no-background" />
+
       <Points styles="vertical first-points" quantity={3}/>
       <Points styles="second-points" quantity={3}/>
       <Points styles="third-points" quantity={3}/>
-      <Wave1 />
-      <p className="html">&lt;html&gt;</p>
+      <Points styles="fourth-points" quantity={2}/>
+      <Points styles="vertical fifth-points" quantity={2}/>
+
       <Code color="#272D37" weight="regular" size={64} className="code"/>
+
+      <Wave1 />
+      <Wave2 />
+      
       <BracketsSquare color="#272D37" weight="regular" size={64} className="array"/>
       <BracketsCurly color="#272D37" weight="regular" size={64} className="object"/>
-      <Circle styles="second-circle reverse"/> 
-      <Points styles="fourth-points" quantity={2}/>
+
       <p className="hello-world">console.log(&quot;Hello World!&quot;)</p>
       <p className="bars">&#x0002F;&#x0002F;&#x0002F;&#x0002F;&#x0002F;&#x0002F;&#x0002F;&#x0002F;&#x0002F;&#x0002F;&#x0002F;</p>
-      <Wave2 />
-      <Circle styles="third-circle no-background"/> 
-      <Points styles="vertical fifth-points" quantity={2}/>
+      <p className="html">&lt;html&gt;</p>
+      
       <Javascript />
       <NextJS />
+      
     </div>
   )
 }
